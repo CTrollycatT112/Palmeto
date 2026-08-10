1794ef1c6f8f803a5f8cb06176ad55da9aae54ca
trollycat <trollycatpersonal@gmail.com>
2026-08-10 17:10:26 -0400

    SERIAL: Instead of duplicating ocde, a better module for now is one file with abstract design. TO BE CHANGED AGAIN..?

--- drivers/src/tty/serial.rs
+++ drivers/src/tty/serial.rs
@@ -2,52 +2,82 @@ use core::fmt::{self, Write};
 use shared::types::status::{KResult, Status};
 use spin::Mutex;
 
-pub mod pl011_uart;
-pub mod s905x_uart;
-
-use pl011_uart::Pl011Uart;
-use s905x_uart::S905xUart;
-
-//
-// Maybe a trait is the better option,
-// but there's no heap allocation yet..
-// An enum pattern should be fine for now
-// TODO:
-//      Maybe use traits?
-//
-pub enum SerialDevice
-{
-    Pl011(Pl011Uart),
-    S905x(S905xUart),
+#[derive(Clone, Copy)]
+pub struct UartConfig {
+    pub dr_offset: u64,
+    pub status_offset: u64,
+    pub tx_full_mask: u32,
 }
 
-impl fmt::Write for SerialDevice {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        match self {
-            SerialDevice::Pl011(uart) => uart.write_str(s),
-            SerialDevice::S905x(uart) => uart.write_str(s),
+impl UartConfig {
+    pub const PL011: Self = Self {
+        dr_offset: 0x00,
+        status_offset: 0x18,
+        tx_full_mask: 1 << 5,
+    };
+
+    pub const S905X: Self = Self {
+        dr_offset: 0x00,
+        status_offset: 0x0C,
+        tx_full_mask: 1 << 21,
+    };
+}
+
+pub struct Uart {
+    vaddr: u64,
+    config: UartConfig,
+}
+
+impl Uart {
+    pub const unsafe fn new(vaddr: u64, config: UartConfig) -> Self {
+        Self { vaddr, config }
+    }
+
+    pub fn write_byte(&self, byte: u8) {
+        let uart_ptr = self.vaddr as *mut u32;
+
+        unsafe {
+            let dr = uart_ptr.add((self.config.dr_offset / 4) as usize);
+            let status = uart_ptr.add((self.config.status_offset / 4) as usize);
+
+            while (core::ptr::read_volatile(status) & self.config.tx_full_mask) != 0 {
+                core::hint::spin_loop();
+            }
+
+            core::ptr::write_volatile(dr, byte as u32);
+        }
+    }
+
+    pub fn write_str_raw(&self, string: &str) {
+        for byte in string.bytes() {
+            if byte == b'\n' {
+                self.write_byte(b'\r');
+            }
+            self.write_byte(byte);
         }
     }
 }
 
-static SERIAL: Mutex<Option<SerialDevice>> = Mutex::new(None);
+impl fmt::Write for Uart {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_str_raw(s);
+        Ok(())
+    }
+}
 
-pub fn init_from_dtb(compatible: &fdt::standard_nodes::Compatible, 
-                     vaddr: u64) -> KResult<()> {
+static SERIAL: Mutex<Option<Uart>> = Mutex::new(None);
+
+pub fn init_from_dtb(compatible: &fdt::standard_nodes::Compatible, vaddr: u64) -> KResult<()> {
     for comp in compatible.all() {
-        let dev = match comp {
-            "arm,pl011" => {
-                Some(SerialDevice::Pl011(unsafe { Pl011Uart::new(vaddr) }))
-            }
-            "amlogic,meson-gx-uart" | "amlogic,meson-s905-uart" => {
-                Some(SerialDevice::S905x(unsafe { S905xUart::new(vaddr) }))
-            }
+        let config = match comp {
+            "arm,pl011" => Some(UartConfig::PL011),
+            "amlogic,meson-gx-uart" | "amlogic,meson-s905-uart" => Some(UartConfig::S905X),
             _ => None,
         };
 
-        if let Some(uart) = dev {
-            let mut lock = SERIAL.lock();
-            *lock = Some(uart);
+        if let Some(cfg) = config {
+            let uart = unsafe { Uart::new(vaddr, cfg) };
+            *SERIAL.lock() = Some(uart);
             return Ok(());
         }
     }
@@ -57,8 +87,7 @@ pub fn init_from_dtb(compatible: &fdt::standard_nodes::Compatible,
 
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
-    let mut lock = SERIAL.lock();
-    if let Some(ref mut serial) = *lock {
+    if let Some(ref mut serial) = *SERIAL.lock() {
         let _ = serial.write_fmt(args);
     }
 }
